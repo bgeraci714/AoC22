9195840d3fafd5819c00296b47651cd93a54b8d0
Blake Geraci <redshirt714@gmail.com>
2022-12-03 14:32:44 -0500

    problem 1 solved

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
+import os
+from pathlib import Path
+PROBLEM = 'B'
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def solve(solution):
+def solve(path, input):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'problem, {solution}')  # Press ⌘F8 to toggle the breakpoint.
+    problem = "A" if "inputA" in path else "B"
+    if problem == "A":
+        return solveA(input)
+    else:
+        return solveB(input)
 
+
+def solveB(input):
+    problem_input = input.split("\n")
+    totals = []
+
+    total = 0
+    for line in problem_input:
+        try:
+            num = int(line)
+            if num == "" or num is None:
+                totals.append(total)
+                total = 0
+            else:
+                total += num
+        except ValueError:
+            totals.append(total)
+            total = 0
+
+    totals.sort(reverse=True)
+    print(totals[0:3])
+    return sum(totals[0:3])
+
+
+def solveA(input):
+    max = -1
+
+    input = input.split("\n")
+
+    sum = 0
+    for line in input:
+        if (line == ""):
+            if sum > max:
+                max = sum
+            sum = 0
+        else:
+            sum += int(line)
+    return max
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    solve('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(os.getcwd())
+    # Open function to open the file "MyFile1.txt"
+    # (same directory) in append mode and
+    path = "problems/1/input" + PROBLEM + ".txt"
+    with open(path) as input:
+        solution = solve(path, input.read())
+        print(solution)
